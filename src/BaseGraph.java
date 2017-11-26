7a18ffdd3483d00b933a3614168d0e7c1f7c0f6b
DongSky <dongsky@live.cn>
2017-11-26 14:32:13 +0800

    uodate Lab 7

--- src/BaseGraph.java
+++ src/BaseGraph.java
@@ -15,6 +15,104 @@ public class BaseGraph {
     boolean[] visited;
     boolean[] walked;
     public ArrayList<String> sequence;
+    
+    class Edge {
+        int weightIndex;
+        boolean walked;
+        int from = 0;
+        int to = 0;
+
+        public Edge(int initWeight, int initFrom, int initTo) {
+          weightIndex = edgeNum;
+          edgeNum += 1;
+          weight[weightIndex] = initWeight;
+          from = initFrom;
+          to = initTo;
+        }
+
+        public void addWeight() {
+          weight[weightIndex] += 1;
+        }
+
+        public int getWeight() {
+          return weight[weightIndex];
+        }
+
+        public int getGlobalIndex() {
+          return weightIndex;
+        }
+
+        public int getFrom() {
+          return from;
+        }
+
+        public int getTo() {
+          return to;
+        }
+      }
+
+      class Vertex {
+        int index;
+        String content;
+        ArrayList<Edge> edges = null;
+
+        public Vertex(String initContent) {
+      
+          content = initContent;
+          edges = new ArrayList<Edge>();
+          index = 0;
+        }
+
+        public int getEdges() {
+          if (edges != null) {
+            return edges.size();
+          } else {
+            return 0;
+          }
+        }
+
+        public void addEdge(int nextIndex) {
+          Edge current = getEdge(nextIndex);
+          if (current == null) {
+            current = new Edge(1, index, nextIndex);
+            edges.add(current);
+          } else {
+            current.addWeight();
+          }
+        }
+
+        public void setIndex(int index) {
+          this.index = index;
+        }
+
+        public void setContent(String newContent) {
+          
+          content = newContent;
+        }
+
+        public String getContent() {
+          return content;
+        }
+
+        public int getIndex() {
+          return index;
+        }
+
+        public Edge getEdge(int nextIndex) {
+          for (int i = 0; i < edges.size(); i++) {
+            Edge currentEdge = edges.get(i);
+            if (currentEdge.getTo() == nextIndex) {
+              return currentEdge;
+            }
+          }
+          return null;
+        }
+      }
+
+      ArrayList<Vertex> vertexList;
+      ArrayList<Edge>[] intestEdge;
+      HashMap<String, Vertex> contentMap;
+    
     public BaseGraph() {   
     }
     public void fileRead(String filename) {
